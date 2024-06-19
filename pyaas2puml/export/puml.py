16223807ab017e51d0b15177784f49101e1410ad
zrgt <garmaev@gmx.de>
2024-06-19 14:52:17 +0200

    Refactor to_puml_content()

--- pyaas2puml/export/puml.py
+++ pyaas2puml/export/puml.py
@@ -1,3 +1,4 @@
+from copy import copy
 from typing import Iterable, List
 
 from pyaas2puml.domain.umlclass import UmlClass
@@ -32,39 +33,55 @@ def to_puml_content(diagram_name: str, uml_items: List[UmlItem], uml_relations: 
     # exports the domain classes and enums
     for uml_item in uml_items:
         if isinstance(uml_item, UmlEnum):
-            uml_enum: UmlEnum = uml_item
-            yield PUML_ITEM_START_TPL.format(item_type='enum', item_fqn=uml_enum.fqn,
-                                             generics=f'<{uml_enum.generics}>' if uml_enum.generics else '')
-            if sort_members:
-                uml_enum.members.sort(key=lambda member: member.name.lower())
-            for member in uml_enum.members:
-                yield PUML_ATTR_TPL.format(visibility="", attr_name=member.name, attr_type=member.value, staticity=FEATURE_STATIC)
-            yield PUML_ITEM_END
+            yield from yeld_puml_enum(uml_item, sort_members)
         elif isinstance(uml_item, UmlClass):
-            uml_class: UmlClass = uml_item
-            yield PUML_ITEM_START_TPL.format(
-                item_type='abstract class' if uml_item.is_abstract else 'class', item_fqn=uml_class.fqn,
-                generics=f'<{uml_class.generics}>' if uml_class.generics else ''
-            )
-            if sort_members:
-                uml_class.attributes.sort(key=lambda attr: attr.name.lower())
-            for uml_attr in uml_class.attributes:
-                if not uml_attr.static:
-                    continue
-                yield PUML_ATTR_TPL.format(
-                    visibility=uml_attr.visibility,
-                    attr_name=uml_attr.name,
-                    attr_type=uml_attr.type,
-                    staticity=FEATURE_STATIC if uml_attr.static else FEATURE_INSTANCE,
-                )
-            yield PUML_ITEM_END
+            yield from yeld_puml_class(uml_item, sort_members)
         else:
             raise TypeError(f'cannot process uml_item of type {uml_item.__class__}')
 
     # exports the domain relationships between classes and enums
     for uml_relation in uml_relations:
         yield PUML_RELATION_TPL.format(
-            source_fqn=uml_relation.source_fqn, rel_type=uml_relation.type.value, target_fqn=uml_relation.target_fqn, label=uml_relation.label
+            source_fqn=uml_relation.source_fqn, rel_type=uml_relation.type.value, target_fqn=uml_relation.target_fqn,
+            label=uml_relation.label
         )
 
     yield PUML_FILE_END
+
+
+def yeld_puml_enum(uml_enum: UmlEnum, sort_members: bool = True) -> Iterable[str]:
+    yield PUML_ITEM_START_TPL.format(item_type='enum', item_fqn=uml_enum.fqn,
+                                     generics=f'<{uml_enum.generics}>' if uml_enum.generics else '')
+    if sort_members:
+        uml_enum.members.sort(key=lambda member: member.name.lower())
+    for member in uml_enum.members:
+        yield PUML_ATTR_TPL.format(visibility="", attr_name=member.name, attr_type=member.value,
+                                   staticity=FEATURE_STATIC)
+    yield PUML_ITEM_END
+
+
+def yeld_puml_class(uml_class: UmlClass, sort_members: bool = True) -> Iterable[str]:
+    yield PUML_ITEM_START_TPL.format(
+        item_type='abstract class' if uml_class.is_abstract else 'class', item_fqn=uml_class.fqn,
+        generics=f'<{uml_class.generics}>' if uml_class.generics else ''
+    )
+    if sort_members:
+        uml_class.attributes.sort(key=lambda attr: attr.name.lower())
+
+    remove_duplicated_attrs(uml_class)
+
+    for uml_attr in uml_class.attributes:
+        yield PUML_ATTR_TPL.format(
+            visibility=uml_attr.visibility,
+            attr_name=uml_attr.name,
+            attr_type=uml_attr.type,
+            staticity=FEATURE_STATIC if uml_attr.static else FEATURE_INSTANCE,
+        )
+    yield PUML_ITEM_END
+
+
+def remove_duplicated_attrs(uml_class: UmlClass) -> UmlClass:
+    static_attrs = [attr.name for attr in uml_class.attributes if attr.static]
+    for attr in copy(uml_class.attributes):
+        if not attr.static and attr.name in static_attrs:
+            uml_class.attributes.remove(attr)
